758f8b4ff59b28d37edb70962ff9ad3b2adea809
Oetam05 <mateoaristizabal@uninorte.edu.co>
2022-10-25 00:46:29 -0500

    Se agregó un dropdown para seleccionar fecha y año

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from dash import dcc
 from dash import html
 import plotly.graph_objs as go
 import pandas as pd
+from datetime import datetime
 
 date_labels = ['fecha reporte web', 'Fecha de notificación', 'Fecha de inicio de síntomas', 'Fecha de muerte', 
           'Fecha de diagnóstico', 'Fecha de recuperación']
@@ -17,15 +18,19 @@ df['Sexo'] = df['Sexo'].replace({'m':'M', 'f':'F'})
 df['Sexo'].unique()
 df['Sexo'] = df['Sexo'].astype('category')
 df['Recuperado'] = df['Recuperado'].replace({'fallecido':'Fallecido'})
-print(df.info())
-print(df.tail())
-pv = pd.pivot_table(df, index=['Nombre departamento'], columns=["Recuperado"], values=['ID de caso'], aggfunc='count', fill_value=0)
 
-trace1 = go.Bar(x=pv.index, y=pv[('ID de caso', 'Activo')], name='Activo')
-trace2 = go.Bar(x=pv.index, y=pv[('ID de caso', 'Recuperado')], name='Recuperado')
-trace3 = go.Bar(x=pv.index, y=pv[('ID de caso', 'Fallecido')], name='Fallecido')
-
-
+mes_options=[]
+m=4
+a=2020
+sw=True
+while(sw):
+    if(m==4 and a==2022):
+        sw=False
+    if(m==13):
+        m=1
+        a+=1
+    mes_options.append(datetime(a, m, 1))
+    m+=1
 
 dash_app = dash.Dash()
 app = dash_app.server
@@ -36,16 +41,42 @@ dash_app.layout = html.Div(children=[
     html.Div(children='''
         Cantidad de Activos, Recuperados y fallecidos por departamento
     '''),
-
+    html.Div(
+        [
+            dcc.Dropdown(
+                id="Fecha",
+                options=[{
+                    'label': str(i.strftime("%B"))+" "+str(i.year),
+                    'value': i
+                } for i in mes_options],
+                ),
+        ],
+        style={'width': '25%',
+               'display': 'inline-block'}),
     dcc.Graph(
-        id='example-graph',
-        figure={
-           'data': [trace1, trace2, trace3],
-            'layout':
-            go.Layout(title='Por departamento', barmode='stack')
-        }
-    )
+        id='grafica-estado',)
 ])
+@dash_app.callback(
+    dash.dependencies.Output('grafica-estado', 'figure'),
+    [dash.dependencies.Input('Fecha', 'value')])
+def update_graph(Fecha):
+    if(not Fecha):        
+        df_plot = df.copy()
+    else:
+        por_a = df[df['Fecha de diagnóstico'].dt.year == int(Fecha[0:4])]
+        df_plot=por_a[por_a['Fecha de diagnóstico'].dt.month==int(Fecha[5:7])]
+    pv = pd.pivot_table(df_plot, index=['Nombre departamento'], columns=["Recuperado"], values=['ID de caso'], aggfunc='count', fill_value=0)
+
+    trace1 = go.Bar(x=pv.index, y=pv[('ID de caso', 'Activo')], name='Activo')
+    trace2 = go.Bar(x=pv.index, y=pv[('ID de caso', 'Recuperado')], name='Recuperado')
+    trace3 = go.Bar(x=pv.index, y=pv[('ID de caso', 'Fallecido')], name='Fallecido')
+    return {
+        'data': [trace1, trace2, trace3],
+        'layout':
+        go.Layout(
+            title='Estado de los pacientes {}'.format(Fecha[0:7] if Fecha else"Siempre"),
+            barmode='stack')
+    }
 
 if __name__ == '__main__':
     dash_app.run_server(debug=True)
